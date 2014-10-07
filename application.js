a24acb3f06bedfe2e611a1a8c27fa8e951574ee6
Katie <kreiner@umich.edu>
2014-10-07 11:30:20 -0400

    changed js

--- application.js
+++ application.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+
+	var font_Size = $("#aboutme").css("fontSize")
 	
 	$('#aboutme').mouseenter(function() {
-		$('#aboutme').stop().animate({ color:'red'});
+		$('#aboutme').stop().animate({fontSize: "20px", color:'red'});
 		$('#aboutme').mouseleave(function() {
-			$('#aboutme').stop().animate({ fontSize : '15px', color:'black'  });
+			$('#aboutme').stop().animate({ fontSize : font_Size , color:'black'  });
 		});
 	});
 });
